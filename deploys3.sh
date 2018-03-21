dd26bdcd92c8a6225cde6f3afbb8e399b6f7628d
Juhannuspukki <jere.juhani@gmail.com>
2018-03-21 21:59:37 +0200

    Add theme to config file

--- deploys3.sh
+++ deploys3.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -xe
 
-STAGE=$1
-
 hugo --theme=pollex
 cd public
 aws s3 sync --delete . s3://pollex.club
-for INDEX in $(find * -name index.html); do
-  [ "$INDEX" == "index.html" ] && continue
-  NEW_INDEX=$(dirname $INDEX)
-  aws s3 mv s3://pollex.club/${STAGE}/$INDEX s3://pollex.club/${STAGE}/${NEW_INDEX}
-done
+aws s3 cp . s3://pollex.club --recursive
