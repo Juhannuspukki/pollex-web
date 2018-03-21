e18c4e68762809d2425dde28d9bf9c8394044fbb
Juhannuspukki <jere.juhani@gmail.com>
2018-03-21 12:47:24 +0200

    Fix typo

--- deploys3.sh
+++ deploys3.sh
@@ -1,23 +1,12 @@
-#!/bin/dash -e
+#!/bin/sh -xe
 
 STAGE=$1
-if [ -z "$STAGE" ]; then
-  STAGE=dev
-fi
 
-hugo --theme=hugo-nel-theme
+hugo --theme=pollex
 cd public
-aws s3 sync --delete . s3://neuroeventlabs.com/${STAGE}/
-
+aws s3 sync --delete . s3://pollex.club
 for INDEX in $(find * -name index.html); do
-  if [ "${INDEX}" = "index.html" ]; then continue; fi
-  NEW_INDEX=$(dirname "${INDEX}")
-  aws s3 mv "s3://neuroeventlabs.com/${STAGE}/${INDEX}" "s3://neuroeventlabs.com/${STAGE}/${NEW_INDEX}"
+  [ "$INDEX" == "index.html" ] && continue
+  NEW_INDEX=$(dirname $INDEX)
+  aws s3 mv s3://pollex.club/${STAGE}/$INDEX s3://pollex.club/${STAGE}/${NEW_INDEX}
 done
-
-case "$STAGE" in
-  "dev") aws cloudfront create-invalidation --distribution-id E1BGBPLIALP0R5 --paths '/*'
-  ;;
-  "www") aws cloudfront create-invalidation --distribution-id E2AUAKAPDCJ120 --paths '/*'
-  ;;
-esac
